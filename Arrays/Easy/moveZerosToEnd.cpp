796d5ab9592d396020063e94f684fc1a3b994ca9
Aswin-04 <e.aswin004@gmail.com>
2024-06-28 06:23:19 +0530

    Solved moveZerosToEnd.cpp

---
+++ Arrays/Easy/moveZerosToEnd.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// TC -- > O(N)
+// SC --> O(1)
+void moveZerosToEnd(int arr[], int n) {
+    int j = -1;
+    for(int i=0; i < n; i++) {
+        if(arr[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    for(int i=j+1; i < n; i++) {
+        if(arr[i] != 0) {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+}
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i < n; i++) {
+        cin >> arr[i];
+    } 
+
+    moveZerosToEnd(arr, n);
+    
+    for(int i=0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+
+    
+    return 0;
+}
