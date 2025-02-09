f2cf2b87bbd7d41aada54080964b622771057deb
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-02-09 18:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (83.65%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xor1 = 0;
+        for(int i: nums) {
+            xor1^=i;
+        }
+        return xor1;
+    }
+};
