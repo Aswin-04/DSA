9a430668f32b09b11070be6f36097dd6d9574666
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-02-13 21:15:13 +0530

    Time: 73 ms (38.77%) | Memory: 47.6 MB (35.75%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> mp;
+        mp[0] = 1;
+        int n = nums.size();
+        int sum=0;
+        int cnt=0;
+        for(int i=0; i < n; i++) {
+            sum+=nums[i];
+            if(mp.find(sum-k) != mp.end()) {
+                cnt+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+
+        return cnt;
+    }
+};
