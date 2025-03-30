089b55a569cb4963a8deaf8289b39c9d6bf64c61
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-03-30 20:22:15 +0530

    Time: 3 ms (76.63%) | Memory: 28.6 MB (38.58%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
+    
+    // TC --> O(4N)
+    // SC --> O(N)
+
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
         stack<int> st;
         vector<int> ans(n, -1);
 
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && st.top() <= nums[i]) {
+        for(int i=n-1; i>=0; i--) {  // O(N)
+            while(!st.empty() && st.top() <= nums[i]) { // O(N) throughout the problem
                 st.pop();
             }
             st.push(nums[i]);
         }
 
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && st.top() <= nums[i]) {
+        for(int i=n-1; i>=0; i--) {  // O(N)
+            while(!st.empty() && st.top() <= nums[i]) { // O(N) throughout the problem
                 st.pop();
             }
             
