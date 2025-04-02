949ef2820bd3f182874cc30d0196ce31b0c901b2
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-04-02 07:22:17 +0530

    Time: 16 ms (82.91%) | Memory: 79.5 MB (92.63%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -6,7 +6,7 @@ public:
         int maxArea = 0;
 
         for(int i=0; i < n; i++) {
-            while(!st.empty() && heights[st.top()] > heights[i]) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
                 int nse = i;
                 int el = st.top();
                 st.pop();
@@ -20,7 +20,6 @@ public:
             int nse = n;
             int el = st.top();
             st.pop();
-            while(!st.empty() && heights[st.top()] == heights[el]) st.pop();
             int pse = st.empty() ? -1 : st.top();
             maxArea = max(maxArea, heights[el]*(nse-pse-1));
         }
