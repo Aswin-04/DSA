b9569a8bc60cd3889cfd3836d5ede88ab7d6e007
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-04-13 06:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (77.11%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -11,22 +11,16 @@
  */
 class Solution {
 public:
-    void updateMax(int& maxSum, int rootVal, int leftSum, int rightSum) {
-        maxSum = max(maxSum, rootVal);
-        maxSum = max(maxSum, rootVal+leftSum);
-        maxSum = max(maxSum, rootVal+rightSum);
-        maxSum = max(maxSum, leftSum+rightSum+rootVal);
-    }
-    int helper(TreeNode* root, int& maxSum) {
+    int maxSum = INT_MIN;
+    int helper(TreeNode* root) {
         if(root == nullptr) return 0;
-        int leftSum = helper(root->left, maxSum);
-        int rightSum = helper(root->right, maxSum);
-        updateMax(maxSum, root->val, leftSum, rightSum);
-        return max(root->val+max(leftSum, rightSum), root->val);
+        int leftSum = max(helper(root->left), 0);
+        int rightSum = max(helper(root->right), 0);
+        maxSum = max(maxSum, root->val+leftSum+rightSum);
+        return root->val + max(leftSum, rightSum);
     }
     int maxPathSum(TreeNode* root) {
-        int maxSum = INT_MIN;
-        helper(root, maxSum);
+        helper(root);
         return maxSum;
     }
 };
