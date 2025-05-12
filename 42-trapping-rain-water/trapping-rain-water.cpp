6b1278f06c67beed555b6667498575bee0cde15e
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-05-12 22:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (8.24%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int l = 0;
-        int r = n-1;
-        int leftMax = height[l];
-        int rightMax = height[r];
-        int total = 0;
+        vector<int> prefix(n, 0);
+        vector<int> suffix(n, 0);
 
-        while(l <= r) {
-            if(leftMax <= rightMax) {
-                int res = leftMax-height[l];
-                total+= (res<0) ? 0 : res;
-                leftMax = max(leftMax, height[l]);
-                l++;
+        int mx = 0;
+        for(int i=0; i < n; i++) {
+            if(mx > height[i]) {
+                prefix[i] = mx;
             }
-
-            else {
-                int res = rightMax-height[r];
-                total+=(res<0) ? 0: res;
-                rightMax = max(rightMax, height[r]);
-                r--;
+            mx = max(mx, height[i]);
+        }
+        mx = 0;
+        for(int i=n-1; i >= 0; i--) {
+            if(mx > height[i]) {
+                suffix[i] = mx;
             }
+            mx = max(mx, height[i]);
         }
 
-        return total;
+        int res = 0;
+        for(int i=0; i < n; i++) {
+            int trapped = min(prefix[i], suffix[i]) - height[i];
+            res+=max(0, trapped);
+        }
+
+        return res;
     }
 };
