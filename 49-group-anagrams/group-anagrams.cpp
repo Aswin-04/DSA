c1811be307ab2b149d319a3d87c9e268331e3bed
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-05-22 19:03:00 +0530

    Time: 40 ms (17.85%) | Memory: 36.1 MB (7.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
-        // TC --> O(M*N)  &   SC --> O(M) extra space and O(M*N) for output list
-
-        unordered_map<string, vector<string>> mpp;
-        for(string s: strs) {
-            vector<int> count(26);
-            for(char ch: s) {
-                count[ch-'a']++;
+        map<vector<int>, vector<string>> anagramHash;
+        for (string s : strs) {
+            vector<int> freq(26, 0);
+            for (char ch : s) {
+                freq[ch - 'a']++;
             }
-            
-            string key = to_string(count[0]);
-            for(int i=1; i < 26; i++) {
-                key+=','+to_string(count[i]);
-            }
-            mpp[key].push_back(s);
+            anagramHash[freq].push_back(s);
         }
 
-        vector<vector<string>> res;
-        for(auto p: mpp) {
-            res.push_back(p.second);
+        vector<vector<string>> answer;
+        for (auto pr : anagramHash) {
+            answer.push_back(pr.second);
         }
 
-        return res;
+        return answer;
     }
 };
