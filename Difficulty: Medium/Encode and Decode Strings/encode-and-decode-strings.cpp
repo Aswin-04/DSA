f7790a80f05d034791de651e82d92c66137533f9
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-05-23 09:11:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.cpp
+++ Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.cpp
@@ -1,71 +1,36 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     string encode(vector<string>& s) {
         // code here
-        string encoded_str="";
-        for(string str: s) {
-            encoded_str+=to_string(str.size()) + "#" + str;
+        string encoded_str = "";
+        for(int i=0; i < s.size(); i++) {
+            encoded_str+=(to_string(s[i].size()) + "#" + s[i]);
         }
         
         return encoded_str;
+        
     }
-    
 
     vector<string> decode(string& s) {
         // code here
         int i=0;
-        vector<string> decoded_str; 
-        while(i < s.size()) {
-            int j=i;
-            while(s[j] != '#') j++;
-            int length = stoi(s.substr(i, j-i));
-            i=j+1;
-            decoded_str.push_back(s.substr(i, length));
-            i+=length;
+        int n = s.size();
+        vector<string> res;
+        
+        while(i < n) {
+            string cnt_str = "";
+            while(s[i] != '#') {
+                cnt_str+=s[i];
+                i++;
+            }
+            
+            i++;
+            int char_count = stoi(cnt_str);
+            res.push_back(s.substr(i, char_count));
+            i+=char_count;
         }
         
-        return decoded_str;
+        return res;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore(); // To ignore the newline after the number of test cases
-
-    while (tc--) {
-        string input;
-        getline(cin, input);
-
-        stringstream ss(input);
-        string word;
-        vector<string> s;
-
-        while (ss >> word) {
-            s.push_back(word);
-        }
-
-        Solution obj;
-        string encodedString = obj.encode(s);
-        vector<string> decodedStrings = obj.decode(encodedString);
-
-        for (string x : decodedStrings) {
-            cout << x << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
