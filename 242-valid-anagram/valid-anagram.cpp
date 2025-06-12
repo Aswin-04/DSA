1b3c38ada05647bb1bace65ebaa45a0e68f0135c
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-12 09:06:53 +0530

    Time: 5 ms (30.53%) | Memory: 9.8 MB (27.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
 
-        vector<int> freq(26, 0);
+        map<char, int> freq1, freq2;
+        for(int i=0; i < s.size(); i++) {
+            freq1[s[i]]++;
+            freq2[t[i]]++;
+        } 
 
-        for(char ch: s) {
-            freq[ch-'a']++;
-        }
-
-        for(char ch: t) {
-            freq[ch-'a']--;
-        }
-
-        for(int i=0; i < 26; i++) {
-            if(freq[i]) return false; 
+        for(int i=0; i < s.size(); i++) {
+            if(freq1[s[i]] != freq2[s[i]]) return false;
         }
 
         return true;
