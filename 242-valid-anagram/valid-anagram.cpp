352825aee1a9d4684915752ae492e132baac87f1
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-12 09:15:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (74.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,15 +3,17 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
 
-        int freq1[26] = {0};
-        int freq2[26] = {0};
+        int freq[26] = {0};
         for(int i=0; i < s.size(); i++) {
-            freq1[s[i]-'a']++;
-            freq2[t[i]-'a']++;
+            freq[s[i]-'a']++;
         } 
 
         for(int i=0; i < s.size(); i++) {
-            if(freq1[s[i]-'a'] != freq2[s[i]-'a']) return false;
+            freq[t[i]-'a']--;
+        } 
+
+        for(int i=0; i < 26; i++) {
+            if(freq[i] != 0) return false;
         }
 
         return true;
