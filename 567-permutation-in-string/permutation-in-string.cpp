521bc864e846e436455fec541412c7714512e06a
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-17 07:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (46.73%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,20 +2,40 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         
-        // TC --> O(n1*n2*n1log(n1)) 
-        // SC --> O(n1);
+        // TC --> O(n2) 
+        // SC --> O(52);
         int n1 = s1.size();
         int n2 = s2.size();
-        sort(s1.begin(), s1.end());
-        for(int i=0; i <= n2-n1; i++) {
-            string str = "";
-            for(int j=i; j < i+n1; j++) {
-                str+=s2[j];
+
+        if(n1 > n2) return false;
+        
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for(int i=0; i < n1; i++) {
+            freq2[s2[i]-'a']++;
+            freq1[s1[i]-'a']++;
+        }
+
+
+        int l=0;
+        int r=n1-1;
+
+        freq2[s2[r]-'a']--;
+        while(r < n2) {
+            freq2[s2[r]-'a']++;
+            int flag = 0;
+            for(int i=0; i < 26; i++) {
+                if(freq1[i] != freq2[i]) {
+                    flag = 1;
+                    break;
+                } 
             }
 
-            sort(str.begin(), str.end());
-
-            if(s1 == str) return true;
+            if(!flag) return true;
+            freq2[s2[l]-'a']--;
+            l++;
+            r++; 
         }
 
         return false;
