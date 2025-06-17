3d8195f0df4cedadb1540fc553513a11ab2c51bc
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-17 10:00:24 +0530

    Time: 52 ms (10.15%) | Memory: 11.6 MB (26.89%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+
+        if(t.size() > s.size()) return "";
+
+        unordered_map<char, int> window, freqT;
+        for(char ch: t) {
+            freqT[ch]++;
+        }
+
+        int l=0;
+        int r=0;
+        int n = s.size();
+        pair<int, int> idx = {-1, n};
+
+        while(r < n) {
+            window[s[r]]++;
+
+            bool valid = true;
+            for(auto pr: freqT) {
+                if(window.find(pr.first) == window.end() ||
+                    window[pr.first] < pr.second
+                ) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if(valid) {
+                while(l <= r) {
+                    if(freqT.find(s[l]) == freqT.end() || 
+                        window[s[l]] > freqT[s[l]]
+                        ) {
+                            window[s[l]]--;
+                            l++;
+                    } 
+                    else break;
+                }
+
+                int crnt_len = idx.second - idx.first +1;
+                int new_len = r-l+1;
+
+                if(new_len < crnt_len) {
+                    idx.first = l;
+                    idx.second = r;
+                }
+            }
+            r++;
+        }
+
+        if(idx.first == -1) return "";
+        return s.substr(idx.first, idx.second-idx.first+1);
+    }
+};
