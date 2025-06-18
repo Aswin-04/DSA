23a491f2b0e63dba652d41a32d8efef865813dc0
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-18 10:07:22 +0530

    Time: 19 ms (24.39%) | Memory: 11.5 MB (66.30%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -5,49 +5,45 @@ public:
         if(t.size() > s.size()) return "";
 
         unordered_map<char, int> window, freqT;
+        int valid = 0;
         for(char ch: t) {
             freqT[ch]++;
+            valid++;
         }
 
         int l=0;
         int r=0;
         int n = s.size();
         pair<int, int> idx = {-1, n};
-
+        int need=0;
         while(r < n) {
             window[s[r]]++;
 
-            if(freqT.find(s[r]) != freqT.end()) {
-                bool valid = true;
-                for(auto pr: freqT) {
-                    if(window.find(pr.first) == window.end() ||
-                        window[pr.first] < pr.second
-                    ) {
-                        valid = false;
-                        break;
-                    }
+            if(window.find(s[r]) != window.end() &&
+               window[s[r]] <= freqT[s[r]]) {
+                need++;
+            }
+               
+            if(need == valid) {
+                while(l <= r) {
+                    if(freqT.find(s[l]) == freqT.end() || 
+                        window[s[l]] > freqT[s[l]]
+                        ) {
+                            window[s[l]]--;
+                            l++;
+                    } 
+                    else break;
                 }
 
-                if(valid) {
-                    while(l <= r) {
-                        if(freqT.find(s[l]) == freqT.end() || 
-                            window[s[l]] > freqT[s[l]]
-                            ) {
-                                window[s[l]]--;
-                                l++;
-                        } 
-                        else break;
-                    }
+                int crnt_len = idx.second - idx.first +1;
+                int new_len = r-l+1;
 
-                    int crnt_len = idx.second - idx.first +1;
-                    int new_len = r-l+1;
-
-                    if(new_len < crnt_len) {
-                        idx.first = l;
-                        idx.second = r;
-                    }
+                if(new_len < crnt_len) {
+                    idx.first = l;
+                    idx.second = r;
                 }
             }
+            
             r++;
         }
 
