2f03b0f21e77fb3c0b35e6c853cc300f62b43211
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-18 08:48:41 +0530

    Time: 69 ms (16.92%) | Memory: 153.3 MB (16.36%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<int> maxHeap;
+
+        // TC --> O(n logn)
+        // SC --> O(n)
+        
+        priority_queue<pair<int, int>> maxHeap;
         unordered_map<int, int> hashWindow;
 
         for(int i=0; i < k; i++) {
-            maxHeap.push(nums[i]);
-            hashWindow[nums[i]] = i;
+            maxHeap.push({nums[i], i});
         }
 
         int l=1;
         int r=k;
         int n=nums.size();
         vector<int> maxWindow;
-        maxWindow.push_back(maxHeap.top());
+        maxWindow.push_back(maxHeap.top().first);
 
         while(r < n) {
-            maxHeap.push(nums[r]);
-            hashWindow[nums[r]] = r;
+            maxHeap.push({nums[r], r});
 
             while(!maxHeap.empty()) {
-                int top = maxHeap.top();
-                int idx = hashWindow[top];
-
+                int idx = maxHeap.top().second;
                 if(idx < l) maxHeap.pop();
                 else break;
             }
 
-            maxWindow.push_back(maxHeap.top());
+            maxWindow.push_back(maxHeap.top().first);
             l++;
             r++;
         }
