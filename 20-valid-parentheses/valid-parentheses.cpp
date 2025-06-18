0518f4eea7b810c36a8f844a22302e22519a4af5
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-18 12:12:04 +0530

    Time: 1 ms (16.54%) | Memory: 9.8 MB (9.69%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool isValid(string s) {
-        unordered_map<char, char> brackets;
-        brackets.insert({')', '('});
-        brackets.insert({'}', '{'});
-        brackets.insert({']', '['});
-
-        stack<char> st;
-        for(char ch: s) {
-            if(ch == '(' || ch == '{' || ch == '[') st.push(ch);
-            else if(st.empty() || st.top() != brackets[ch]) return false;
-            else st.pop();
+        // TC --> O(n)
+        // SC --> O(n)
+        if(s.size()&1) return false;
+        stack<char> char_stk;
+        for(int i=0; i < s.size(); i++) {
+            if(s[i] == '(') char_stk.push(')');
+            else if(s[i] == '{') char_stk.push('}');
+            else if(s[i] == '[') char_stk.push(']');
+            else if(!char_stk.empty() && s[i] == char_stk.top()) char_stk.pop();
+            else return false;
         }
-
-        return st.empty();
+        
+        return char_stk.empty() ? true : false;
     }
 };
