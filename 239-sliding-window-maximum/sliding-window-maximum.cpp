65d3476b921b231ebc758f94024126c5a4d1494e
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-18 08:54:30 +0530

    Time: 72 ms (15.95%) | Memory: 153.1 MB (19.18%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,34 +4,19 @@ public:
 
         // TC --> O(n logn)
         // SC --> O(n)
-        
+
         priority_queue<pair<int, int>> maxHeap;
-        unordered_map<int, int> hashWindow;
-
-        for(int i=0; i < k; i++) {
-            maxHeap.push({nums[i], i});
-        }
-
-        int l=1;
-        int r=k;
-        int n=nums.size();
         vector<int> maxWindow;
-        maxWindow.push_back(maxHeap.top().first);
 
-        while(r < n) {
-            maxHeap.push({nums[r], r});
-
-            while(!maxHeap.empty()) {
-                int idx = maxHeap.top().second;
-                if(idx < l) maxHeap.pop();
-                else break;
+        for(int i=0; i < nums.size(); i++) {
+            maxHeap.push({nums[i], i});
+            if(i >= k-1) {
+                while(maxHeap.top().second < i-k+1) {
+                    maxHeap.pop();
+                }
+                maxWindow.push_back(maxHeap.top().first);
             }
-
-            maxWindow.push_back(maxHeap.top().first);
-            l++;
-            r++;
         }
-
         return maxWindow;
     }
 };
