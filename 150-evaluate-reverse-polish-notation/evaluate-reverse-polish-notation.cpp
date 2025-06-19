d25aeccd7be85462f84f5c6a74b028078f192868
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-19 05:30:11 +0530

    Time: 6 ms (11.22%) | Memory: 17.2 MB (16.66%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    int my_stoi(string num) {
+        int result = 0;
+        int sign = 1;
+        int i=0;
+
+        if(num[i] == '-') {
+            sign = -1;
+            i++;
+        }
+
+        else if(num[i] == '+') {
+            i++;
+        }
+
+        for(; i < num.size(); i++) {
+            int digit = num[i] - '0';
+            result = (result*10) + digit;
+        }
+
+        return sign*result;
+    }
+    int eval_exp(int val1, char arth_optr, int val2) {
+        switch(arth_optr) {
+            case '+':
+                return val1 + val2;
+            case '-':
+                return val1 - val2;
+            case '*':
+                return val1 * val2;
+            case '/':
+                return val1 / val2;
+        }
+        return -1;
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<int> nums_stk;
+        int ans = 0;
+        for(string s: tokens) {
+            if(s == "+" || s == "-" || s == "*" || s == "/") {
+                int val2 = nums_stk.top(); nums_stk.pop();
+                int val1 = nums_stk.top(); nums_stk.pop();
+                ans = eval_exp(val1, s[0], val2);
+                nums_stk.push(ans);
+            }
+            else nums_stk.push(my_stoi(s));
+        }
+
+        return nums_stk.empty() ? ans : nums_stk.top();
+    }
+};
