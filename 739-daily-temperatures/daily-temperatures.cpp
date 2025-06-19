a55bda67e645a6d8044f861d9620f2582d33d368
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-19 06:29:34 +0530

    Time: 23 ms (58.39%) | Memory: 102.7 MB (91.52%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int> stk;
+        // NGE
         int n = temperatures.size();
-        vector<int> answer(n);
-
-        for(int i=n-1; i >= 0; i--) {
-            while(!stk.empty() && temperatures[stk.top()] <= temperatures[i]) {
-                stk.pop();
+        stack<int> nge_stk;
+        vector<int> answer(n, 0);
+        for(int i=n-1;i >= 0; i--) {
+            while(!nge_stk.empty() && temperatures[i] >= temperatures[nge_stk.top()]) {
+                nge_stk.pop();
             }
-
-            if(stk.empty()) answer[i] = 0;
-            else answer[i] = stk.top() - i;
-            stk.push(i);
+            
+            if(!nge_stk.empty()) answer[i] = nge_stk.top() - i;
+            nge_stk.push(i);
         }
-
         return answer;
     }
 };
