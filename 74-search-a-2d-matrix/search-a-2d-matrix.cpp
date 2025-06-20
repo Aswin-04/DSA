c5cf821a86981add8d63090f477b3a21674c4349
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-20 07:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (97.83%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // binary search to find the row, where the target may exist
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int l=0, r = m-1;
+        int row = -1;
+        while(l <= r) {
+            int mid = (l+r)/2;
+            int lb = matrix[mid][0];
+            int ub = matrix[mid][n-1];
+
+            if(target < lb) r = mid-1;
+            else if(target > ub) l = mid+1;
+            else  {
+                row = mid;
+                break;
+            }
+        }
+
+        if(row == -1) return false;
+        l=0; r = n-1;
+        while(l <= r) {
+            int mid = (l+r)/2;
+            if(matrix[row][mid] == target) return true;
+            else if(matrix[row][mid] < target) l=mid+1;
+            else r=mid-1;
+        }
+
+        return false;
+    }
+};
