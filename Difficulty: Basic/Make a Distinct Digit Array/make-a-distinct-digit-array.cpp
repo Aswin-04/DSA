574b0fd87ec3e22a262bf2f4b05a053355d2efe4
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-24 07:31:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Make a Distinct Digit Array/make-a-distinct-digit-array.cpp
+++ Difficulty: Basic/Make a Distinct Digit Array/make-a-distinct-digit-array.cpp
@@ -3,16 +3,23 @@ class Solution {
   public:
     vector<int> common_digits(vector<int> nums) {
         // Code here
-        set<int> st;
-        for(int i=0; i < nums.size(); i++) {
-            int num = nums[i];
-            while(num > 0) {
-                st.insert(num%10);
-                num/=10;
-            }
+        vector<int> digitsHash(10, 0);
+        vector<int> answer;
+        
+        for(int val: nums) {
+        int temp = val;
+        while(temp) {
+          int rem = temp%10;
+          digitsHash[rem] = 1;
+          temp/=10;
+        }
         }
         
-        return vector<int>(st.begin(), st.end());
+        for(int i=0; i < digitsHash.size(); i++) {
+        if(digitsHash[i] == 0) continue;
+        answer.push_back(i); 
+        }
         
+        return answer;
     }
 };
