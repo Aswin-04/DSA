02dace85f5d350f788fc9d7e1b4d716857c7734c
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-06-25 10:53:28 +0530

    Time: 1 ms (46.58%) | Memory: 77.2 MB (12.18%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,54 +11,30 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ptr1 = l1;
-        ListNode* ptr2 = l2;
+        
         ListNode* dummy = new ListNode();
-        ListNode* l3 = dummy;
-
+        ListNode* cur = dummy;
         int carry = 0;
-        while(ptr1 && ptr2) {
-            int sum = ptr1->val + ptr2->val + carry;
-            int val = sum%10;
-            carry = sum/10;
+
+        while(l1 != nullptr || l2 != nullptr || carry != 0) {
+
+            int v1 = (l1 != nullptr) ? l1->val : 0;
+            int v2 = (l2 != nullptr) ? l2->val : 0;
+
+            int val = v1+v2+carry;
+            carry = val/10;
+            val = val%10;
 
             ListNode* newNode = new ListNode(val);
-            l3->next = newNode;
+            cur->next = newNode;
 
-            l3 = l3->next;
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
+            cur = cur->next;
+            if(l1 != nullptr) l1 = l1->next;
+            if(l2 != nullptr) l2 = l2->next;
         }
 
-        while(ptr1) {
-            int sum = ptr1->val + carry;
-            int val = sum%10;
-            carry = sum/10;
-
-            ListNode* newNode = new ListNode(val);
-            l3->next = newNode;
-
-            l3 = l3->next;
-            ptr1 = ptr1->next;
-        }
-
-        while(ptr2) {
-            int sum = ptr2->val + carry;
-            int val = sum%10;
-            carry = sum/10;
-
-            ListNode* newNode = new ListNode(val);
-            l3->next = newNode;
-
-            l3 = l3->next;
-            ptr2 = ptr2->next;
-        }
-
-        if(carry) {
-            ListNode* newNode = new ListNode(carry);
-            l3->next = newNode;
-        }
-
-        return dummy->next;
+        ListNode* res = dummy->next;
+        delete dummy;
+        return res;
     }
 };
