ee5452bf48187e5e80be34b8f798e64902c338a0
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-08-12 06:48:32 +0530

    Time: 4 ms (43.20%) | Memory: 14.1 MB (54.25%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> comb;
         vector<vector<int>> res;
-        sort(candidates.begin(), candidates.end());
         makeCombination(0, target, comb, candidates, res);
         return res;
     }
@@ -11,20 +10,16 @@ public:
 private:
     void makeCombination(int idx, int target, vector<int>& comb, vector<int>& candidates, vector<vector<int>>& res) {
 
+        if(idx == candidates.size() || target < 0) return;
+
         if(target == 0) {
             res.push_back(comb);
             return;
         }
 
-        for(int i=idx; i < candidates.size(); i++) {
-            if(candidates[i] > target) {
-                for(auto it: comb) cout << it << " ";
-                cout << endl;
-                return;
-            }
-            comb.push_back(candidates[i]);
-            makeCombination(i, target-candidates[i], comb, candidates, res);
-            comb.pop_back();
-        }
+        comb.push_back(candidates[idx]);
+        makeCombination(idx, target-candidates[idx], comb, candidates, res);
+        comb.pop_back();   
+        makeCombination(idx+1, target, comb, candidates, res);  
     }
 };
