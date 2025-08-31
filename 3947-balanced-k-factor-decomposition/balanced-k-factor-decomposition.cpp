e8878c894d990661c1eacff4f3f1005e7b1f9b3f
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-08-31 20:52:22 +0530

    Time: 12 ms (67.83%) | Memory: 9.9 MB (58.44%) - LeetSync

--- 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
+++ 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
@@ -5,8 +5,10 @@ public:
         vector<int> chosen;
         vector<int> ans;
         int min_diff = INT_MAX;
+
         get_divisors(n, divs);
         backtrack(0, n, k, 1, min_diff, chosen, divs, ans);
+
         return ans;
     }
 
@@ -27,23 +29,27 @@ private:
         return maxi - mini;
     }
 
-    void backtrack(int idx, int n, int k, int product, int min_diff, vector<int>& chosen, vector<int> divs, vector<int>& ans) { 
+    void backtrack(int idx, int n, int k, int product, int& min_diff, vector<int>& chosen, vector<int>& divs, vector<int>& ans) { 
         // pruning
 
         // base case
         if(chosen.size() == k) {
             if(product == n) {
-                if(get_diff(chosen) < min_diff) ans = chosen;
+                int diff = get_diff(chosen);
+                if(diff < min_diff) {
+                    ans = chosen;
+                    min_diff = diff;
+                }
             }
             return;
         }
 
         // choice
         for(int i=idx; i < divs.size(); i++) {
-            if(1ll*product*divs[i] > n) break;
-            chosen.push_back(divs[i]);
-            backtrack(i, n, k, product*divs[i], min_diff, chosen, divs, ans);
-            chosen.pop_back();
+            if(1ll*product*divs[i] > n) break;  // prune
+            chosen.push_back(divs[i]); // insert and move to next 
+            backtrack(i, n, k, product*divs[i], min_diff, chosen, divs, ans); // move
+            chosen.pop_back(); // revert
         }
     }
 };
