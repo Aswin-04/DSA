7c3cd27805bc113bff2cbff70c150b7fcdaa062d
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-08-31 20:42:30 +0530

    Time: 294 ms (17.24%) | Memory: 364.7 MB (5.00%) - LeetSync

---
+++ 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> minDifference(int n, int k) {
+        vector<int> divs;
+        vector<int> chosen;
+        vector<int> ans;
+        int min_diff = INT_MAX;
+        get_divisors(n, divs);
+        backtrack(0, n, k, 1, min_diff, chosen, divs, ans);
+        return ans;
+    }
+
+private: 
+    void get_divisors(int n, vector<int>& divs) {
+        for(int i=1; i*i <= n; i++) {
+            if(n%i == 0) {
+                divs.push_back(i);
+                if(i != n/i) divs.push_back(n/i);
+            }
+        }
+        sort(divs.begin(), divs.end());
+    }
+
+    int get_diff(vector<int>& chosen) {
+        int maxi = *max_element(chosen.begin(), chosen.end());
+        int mini = *min_element(chosen.begin(), chosen.end());
+        return maxi - mini;
+    }
+
+    void backtrack(int idx, int n, int k, int product, int min_diff, vector<int>& chosen, vector<int> divs, vector<int>& ans) { 
+        // pruning
+
+        // base case
+        if(chosen.size() == k) {
+            if(product == n) {
+                if(get_diff(chosen) < min_diff) ans = chosen;
+            }
+            return;
+        }
+
+        // choice
+        for(int i=idx; i < divs.size(); i++) {
+            if(1ll*product*divs[i] > n) break;
+            chosen.push_back(divs[i]);
+            backtrack(i, n, k, product*divs[i], min_diff, chosen, divs, ans);
+            chosen.pop_back();
+        }
+    }
+};
