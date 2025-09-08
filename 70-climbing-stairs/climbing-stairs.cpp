51247c93ae31b86a07a8a47f83ea0d86bedd970a
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-09-08 07:22:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (52.93%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1);
-        dp[0] = dp[1] = 1;
-
-        for(int i=2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        vector<int> dp(n+1, -1);
+        return recc(n, dp);
     }
+private:
+    int recc(int level, vector<int>& dp) {
+        
+        // pruning
+        if(level < 0) return 0;
 
+        // basecase
+        if(level == 0 || level == 1) return 1;
+
+        // cache
+        if(dp[level] != -1) return dp[level];
+
+        // transition
+        return dp[level] = recc(level-1, dp) + recc(level-2, dp);
+    }
 };
