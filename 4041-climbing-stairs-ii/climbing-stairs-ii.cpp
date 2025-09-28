c7155fa5f5d444f3e060f3240a6ecdf1e6f869ec
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-09-28 12:31:09 +0530

    Time: 57 ms (5.32%) | Memory: 187.2 MB (12.50%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int climbStairs(int n, vector<int>& costs) {
         vector<int> dp(n+1, -1);
-        int v = recc(n, costs, dp);
-        return v;
+        return recc(n, costs, dp);
     }
 
     int recc(int n, vector<int>& costs, vector<int>& dp) {
-        if(n < 0) return -1;
+        if(n < 0) return INT_MAX;
         if(n == 0) return 0;
 
         if(dp[n] != -1) return dp[n];
@@ -15,7 +14,7 @@ public:
         int res = INT_MAX;
         for(int i=1; i <= 3; i++) {
             int rv = recc(n-i, costs, dp);
-            if(rv != -1) {
+            if(rv != INT_MAX) {
                 res = min(res, costs[n-1] + (i*i) + rv);
             }
         }
