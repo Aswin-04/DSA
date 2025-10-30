9251819489fd317c9fe71942323085a86a872fb8
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-10-30 19:31:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find Prime numbers in a range/find-prime-numbers-in-a-range.cpp
+++ Difficulty: Medium/Find Prime numbers in a range/find-prime-numbers-in-a-range.cpp
@@ -5,13 +5,13 @@ class Solution {
     vector<int> primeRange(int M, int N) {
         // code here
         vector<int> prime(N+1, 1);
-        prime[0] = 0;
-        prime[1] = 0;
+        prime[0] = prime[1] = 0;
         
         for(int i=2; i <= N; i++) {
-            if(prime[i] == 0) continue;
-            for(long long j=1LL*i*i; j <= N; j+=i) {
-                prime[j] = 0;
+            if(prime[i]) {
+                for(long long j=1LL*i*i; j <= N; j+=i) {
+                    prime[j] = 0;
+                }
             }
         }
         
