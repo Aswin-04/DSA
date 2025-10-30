ed35a252798b32a5d0c78e9d3fb189a0882c0eb5
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-10-30 18:37:59 +0530

    Time: 3 ms (45.88%) | Memory: 8.4 MB (92.14%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        
+        if(x < 0) return false;
         int num = x;
-        int rev_num = 0;
-        if(num < 0) return false;
-        while(num > 0) {
-            int ld = num%10;
-            if((1LL*rev_num*10)+ld > INT_MAX || (1LL*rev_num*10)+ld < INT_MIN) return false;
-            rev_num = rev_num*10+ld;
+        int ans = 0;
+
+        while(num) {
+            if(ans > INT_MAX/10) return false;
+            ans = (ans*10) + (num%10);
             num/=10;
-        } 
-        cout << rev_num;
-        return rev_num == x;
+        }
+
+        return ans == x;
     }
 };
