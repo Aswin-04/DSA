7f5be39ff32d79a4cfb190033be959cf4c5e4fee
Aswin <133287527+Aswin-04@users.noreply.github.com>
2025-10-30 18:49:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+bool prime(int n) {
+
+    if(n == 1) return false;
+    if(n == 2) return true;
+    if(n%2 == 0) return false;
+    
+    for(int i=3; i < n; i+=2) {
+        if(n%i == 0) return false;
+    }
+    
+    return true;
+}
