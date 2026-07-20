7d2f3ab38d7a2df81d9bcfa20e46179166a643b8
Aswin <133287527+Aswin-04@users.noreply.github.com>
2026-07-20 21:21:30 +0530

    Time: 58 ms (5.16%) | Memory: 46.5 MB (5.16%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+
+        while(k--) {
+            vector<vector<int>> ans(n, vector<int>(m));
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<m-1; j++) {
+                    ans[i][j+1] = grid[i][j];
+                }
+                ans[(i+1)%n][0] = grid[i][m-1];
+            }
+            grid = ans;
+        }
+        return grid;
+    }
+};
