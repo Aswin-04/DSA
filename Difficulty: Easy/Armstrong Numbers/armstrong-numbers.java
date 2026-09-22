6c8d87b73d0ca9fda4429c08f8224dd6ab41c1d2
Aswin <133287527+Aswin-04@users.noreply.github.com>
2026-09-22 13:10:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -1,19 +1,15 @@
-// User function Template for Java
 class Solution {
-    private static int countDigits(int n) {
-        return (int) Math.log10(n) + 1;
-    }
     static boolean armstrongNumber(int n) {
         // code here
+        int num = n;
         int res = 0;
-        int temp = n;
-        int exp = countDigits(n);
-        while(temp != 0) {
-            int base = temp % 10;
-            res+= Math.pow(base, exp);
-            temp/=10;
+        
+        while(num != 0) {
+            int ld = num%10;
+            res = res + (ld*ld*ld);
+            num/=10;
         }
         
-        return res == n;
+        return n == res;
     }
 }
