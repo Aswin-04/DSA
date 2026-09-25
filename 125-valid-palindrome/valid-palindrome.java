94de71c3dc4f57e562d60e73e2e34ea5b67da355
Aswin <133287527+Aswin-04@users.noreply.github.com>
2026-09-25 09:47:01 +0530

    Time: 4 ms (43.73%) | Memory: 45.1 MB (37.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,21 @@
 class Solution {
-
-    private boolean checkPalindrome(StringBuilder s, int i) {
-
-        int j = s.length()-i-1;
-        if(i >= j) return true;
-        if(s.charAt(i) != s.charAt(j)) return false;
-        return checkPalindrome(s, i+1);
-    }
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i < s.length(); i++) {
-            if(Character.isLetterOrDigit(s.charAt(i))) {
-                sb.append(Character.toLowerCase(s.charAt(i)));
+        for(char ch: s.toCharArray()) {
+            if(Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
             }
         }
-        return checkPalindrome(sb, 0);
+
+        return checkPalindrome(sb);
+    }
+
+    private boolean checkPalindrome(StringBuilder s) {
+        int n = s.length();
+        for(int i=0; i < n/2; i++) {
+            if(s.charAt(i) != s.charAt(n-i-1)) return false;
+        }
+
+        return true;
     }
 }
