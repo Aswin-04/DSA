ce714625b2912b7e66e49f5cd2ba128497084377
Aswin <133287527+Aswin-04@users.noreply.github.com>
2026-09-26 14:19:21 +0530

    Time: 9 ms (88.22%) | Memory: 46.4 MB (76.49%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -2,26 +2,27 @@ class Solution {
     public String frequencySort(String s) {
 
         int[] freq = new int[128];
-        for(int i=0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i)]++;
         }
 
         HashMap<Integer, StringBuilder> bucket = new HashMap<>();
-        for(int i=0; i < 128; i++) {
-            if(freq[i] == 0) continue;
+        for (int i = 0; i < 128; i++) {
+            if (freq[i] == 0)
+                continue;
             bucket.putIfAbsent(freq[i], new StringBuilder());
-            bucket.get(freq[i]).append((char)i);
+            bucket.get(freq[i]).append((char) i);
         }
 
         StringBuilder res = new StringBuilder();
-        for(int i=s.length(); i > 0; i--) {
-            if(bucket.containsKey(i)) {
+        for (int i = s.length(); i > 0; i--) {
+            if (bucket.containsKey(i)) {
                 StringBuilder str = bucket.get(i);
-                for(int j=0; j < str.length(); j++) {
+                for (int j = 0; j < str.length(); j++) {
                     res.append(String.valueOf(str.charAt(j)).repeat(i));
                 }
             }
-        } 
+        }
 
         return res.toString();
     }
